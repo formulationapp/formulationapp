7d9e4d48d32e3c48f12257f5a5f4c9f859506893
Mateusz Woźniak <matisiek11@gmail.com>
2023-09-18 21:43:37 +0200

    Added scaff for dashboard

--- client/src/views/Register/RegisterView.vue
+++ client/src/views/Register/RegisterView.vue
@@ -34,13 +34,13 @@ async function onSubmit(event: Event) {
   <div
       class="container relative hidden h-full flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
     <a
-        href="/register"
+        href="/login"
         :class="cn(
         buttonVariants({ variant: 'ghost' }),
         'absolute right-4 top-4 md:right-8 md:top-8',
       )"
     >
-      Create an account
+      Sign in
     </a>
     <div class="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
       <div class="absolute inset-0 bg-zinc-900"/>
@@ -74,10 +74,10 @@ async function onSubmit(event: Event) {
       <div class="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
         <div class="flex flex-col space-y-2 text-center">
           <h1 class="text-2xl font-semibold tracking-tight">
-            Sign in
+            Create an account
           </h1>
           <p class="text-sm text-muted-foreground">
-            Enter your email below to sign in to dashboard
+            Enter your email below to create an account
           </p>
         </div>
         <div :class="cn('grid gap-6', $attrs.class ?? '')">
@@ -108,19 +108,33 @@ async function onSubmit(event: Event) {
                 />
               </div>
               <Button :disabled="isLoading" class="mt-2">
-                <!--                <LucideSpinner v-if="isLoading" class="mr-2 h-4 w-4 animate-spin"/>-->
-                Sign In
+                Create an account
               </Button>
             </div>
           </form>
         </div>
         <p class="px-8 text-center text-sm text-muted-foreground">
-          <a href="/register"
-             class="underline underline-offset-4 hover:text-primary">
-            Create an account
+          By clicking continue, you agree to our
+          <a
+              href="/terms"
+              class="underline underline-offset-4 hover:text-primary"
+          >
+            Terms of Service
           </a>
+          and
+          <a
+              href="/privacy"
+              class="underline underline-offset-4 hover:text-primary"
+          >
+            Privacy Policy
+          </a>
+          .
         </p>
       </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+
+</style>
